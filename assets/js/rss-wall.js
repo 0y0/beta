11b096e97e08934fed8bd920dab44b15d01af540
0y0 <ask@oyo.tokyo>
2020-10-10 12:47:04 +0900

    Quick push

--- assets/js/rss-wall.js
+++ assets/js/rss-wall.js
@@ -1,26 +1,26 @@
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const RSS_URL = "https://news.yahoo.co.jp/rss/media/shokuhin/all.xml";
 
-fetch(proxyurl + RSS_URL)
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => {
-    const items = data.querySelectorAll("item");
-    let html = ``;
-    items.forEach(el => {
-      html += `
-        <article>
-          <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
-            <img src="${el.querySelector("image").innerHTML}" alt="">
-            <h2>
-${el.querySelector("title").innerHTML}
-            </h2>
-          </a>
-        </article>
-      `;
-    });
-    //document.body.insertAdjacentHTML("beforeend", html);
-    //document.getElementById("main").innerHTML = html;
-    document.body.innerHTML = html;
-  });
-
+function fetch_rss(links) {
+  for (var url of links) {
+    fetch(proxyurl + url)
+      .then(response => response.text())
+      .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+      .then(data => {
+        const items = data.querySelectorAll("item");
+        let html = ``;
+        items.forEach(el => {
+          html += `
+            <article>
+              <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+          `;
+          var image = el.querySelector("image");
+          if (image) html += `<img src="${image.innerHTML}" alt="">`;
+          html += `<h2>${el.querySelector("title").innerHTML}</h2>
+              </a>
+            </article>
+          `;
+        });
+        document.body.insertAdjacentHTML('beforeend', html);
+      });
+  }
+}
