3b1345b82064385a60a32f0534762a84bf7e4370
0y0 <ask@oyo.tokyo>
2020-10-10 15:02:24 +0900

    Quick push

--- assets/js/rss-wall.js
+++ assets/js/rss-wall.js
@@ -1,26 +1,60 @@
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-function fetch_rss(links) {
-  for (var i = 0; i < links.length; i++) {
-    fetch(proxyurl + links[i])
-      .then(response => response.text())
-      .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-      .then(data => {
-        const items = data.querySelectorAll("item");
-        let html = ``;
-        items.forEach(el => {
-          html += `
-            <article>
-              <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
-          `;
-          var image = el.querySelector("image");
-          if (image) html += `<img src="${image.innerHTML}" alt="">`;
-          html += `<h2>${el.querySelector("title").innerHTML}</h2>
-              </a>
-            </article>
-          `;
-        });
-        document.body.insertAdjacentHTML('beforeend', html);
+const htmlEntities = {
+  nbsp: ' ',
+  cent: '¢',
+  pound: '£',
+  yen: '¥',
+  euro: '€',
+  copy: '©',
+  reg: '®',
+  lt: '<',
+  gt: '>',
+  quot: '"',
+  amp: '&',
+  apos: '\''
+};
+
+function unescapeHTML(str) {
+  return str.replace(/\&([^;]+);/g, function (entity, entityCode) {
+    var match;
+    if (entityCode in htmlEntities)
+      return htmlEntities[entityCode];
+    else if (match = entityCode.match(/^#x([\da-fA-F]+)$/))
+      return String.fromCharCode(parseInt(match[1], 16));
+    else if (match = entityCode.match(/^#(\d+)$/))
+      return String.fromCharCode(~~match[1]);
+    else
+      return entity;
+  });
+};
+
+function async_fetch(link) {
+  fetch(proxyurl + url)
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+      const items = data.querySelectorAll("item");
+      let html = ``;
+      items.forEach(el => {
+        html += `
+          <article>
+            <a href="${el.querySelector("link").innerHTML}" target="_blank" rel="noopener">
+        `;
+        var image = el.querySelector("image");
+        var title = el.querySelector("title");
+        if (image) html += `<img src="${image.innerHTML}" alt="">`;
+        html += `<h2>${unescapeHTML(title.innerHTML)}</h2>
+            </a>
+          </article>
+        `;
       });
+      document.body.insertAdjacentHTML('beforeend', html);
+    });
+}
+
+async function fetch_rss(links) {
+  for (url of links) {
+    await async_fetch(url);
   }
 }
